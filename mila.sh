621849b2efc68a0816ac466428606f5d7ba86d8a
Michael Noukhovitch <noukhovm@mila.quebec>
2025-09-05 17:40:01 -0400

    mila install

---
+++ mila.sh
@@ -0,0 +1,7 @@
+export WANDB_ENTITY=mnoukhov
+export WANDB_PROJECT=open-instruct
+export HF_HOME=$SCRATCH/huggingface
+export UV_CACHE_DIR=$SCRATCH/.cache/uv
+module load python/3.10
+module load cuda/12.6.0/cudnn/9.3
+
